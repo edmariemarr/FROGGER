1e2875811c0963eb14c5f13f05d4d43bdc52495a
Edmarie <42824021+edmariemarr@users.noreply.github.com>
2021-05-27 00:30:19 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ class Frog:
 
     def create(self, win):  # create frog
         self.frog.draw(win)
-        win.update()
 
     def undraw(self):  # undraw frog
         self.frog.undraw()
@@ -36,8 +35,8 @@ class Frog:
         return int(right)
 
     def collision(self, other):     # if frog and car collide
-        if self.x < other.x + other.getWidth() and self.x + self.getWidth() > other.x and \
-                self.y < other.y + other.getHeight() and self.y + self.getHeight() > other.y:
+        if self.getX() < other.getX() + other.getWidth() and self.getX() + self.getWidth() > other.getX() and \
+                self.getY() < other.getY() + other.getHeight() and self.getX() + self.getHeight() > other.getY():
             return True
         return False
 
@@ -81,11 +80,25 @@ class Car:
         right = self.car.getAnchor().getX() + self.car.getWidth() / 2
         return int(right)
 
-    def direction(self):
-        if self.speed < 0:
-            return "left"
-        elif self.speed > 0:
-            return "right"
+class Home:
+    def __init__(self, x, y, win):
+        self.x = x
+        self.y = y
+        self.win = win
+        self.pad = Image(Point(self.x, self.y), "/img/lilypad.png")     # sets image of lilypad
+        self.pad.draw(win)
+
+    def getWidth(self):  # gets width of lilypad
+        return self.pad.getWidth()
+
+    def getHeight(self):  # gets height of lilypad
+        return self.pad.getHeight()
+
+    def getX(self):  # receives x value
+        return self.x
+
+    def getY(self):  # receives y value
+        return self.y
 
 def main():
     WIDTH = 600
@@ -93,6 +106,19 @@ def main():
     win = GraphWin("FROGGER", WIDTH, HEIGHT)  # sets window
     win.setBackground("black")
     win.setCoords(0, 0, 13, 13)
+    homeline = Line(Point(0, 10), Point(13, 10))    # sets home line / where frog must go
+    homeline.setFill("white")
+    homeline.draw(win)
+    baseline = Line(Point(0, 4), Point(13, 4))      # sets base line / where frog begins
+    baseline.setFill("white")
+    baseline.draw(win)
+    #   HOME
+    pads = [Home(1.5, 11.5, win),
+            Home(3.5, 11.5, win),
+            Home(5.5, 11.5, win),
+            Home(7.5, 11.5, win),
+            Home(9.5, 11.5, win),
+            Home(11.5, 11.5, win)]
 
     #   CARS
     myCars = [Car(6.5, 7, "/img/car.png", -2, win),
@@ -115,18 +141,17 @@ def main():
         if key:  # FROG MOVEMENT WITH WASD KEYS
             froggerPlayer.undraw()
             if key == 'w':
-                y_frog += 1.9
+                y_frog += 0.9
                 froggerPlayer.frog.move(0, y_frog)
             elif key == 'a':
-                x_frog -= 1.9
+                x_frog -= 0.9
                 froggerPlayer.frog.move(x_frog, 0)
             elif key == 's':
-                y_frog -= 1.9
+                y_frog -= 0.9
                 froggerPlayer.frog.move(0, y_frog)
             elif key == 'd':
-                x_frog += 1.9
+                x_frog += 0.9
                 froggerPlayer.frog.move(x_frog, 0)
-            froggerPlayer = Frog(x_frog, y_frog, win)
             froggerPlayer.create(win)
 
         #   CARS MOVEMENT
@@ -147,12 +172,15 @@ def main():
 
         #   FROG REACHES HOME
         #   NEED TO WORK ON TEXT APPEARANCE
-        if froggerPlayer.getHeight() >= win.getHeight():
-            time.sleep(5)
-            gameOver = True
+        for lilypads in pads:
+            if froggerPlayer.collision(lilypads):
+                winning = Text(Point(6.5, 1.5), "FROGGER IS HOME SAFE!")
+                winning.setTextColor("white")
+                winning.draw(win)
+                time.sleep(5)
+                gameOver = True
 
     if gameOver:  # CLOSE GAME
         win.close()
-
 
 main()
